068e07023060ebdbd5be8e89b174d7a02216a9af
guramnozadze <gukanozadze@gmail.com>
2026-06-25 20:10:44 +0400

    handling int, float and string (d, f, s) in minprintf

--- main.c
+++ main.c
@@ -4,10 +4,42 @@
 /* minprintf: minimal printf with variable argument list */
 void minprintf(char *fmt, ...) {
     va_list ap; /* points to each unnamed arg in turn */
-        char *p, *sval;
+    char *p, *sval;
+
+    int ival;
+    double dval;
+
+    va_start(ap, fmt);
+    for(p = fmt; *p; p++) {
+        if (*p != '%') {
+            putchar(*p);
+            continue;
+        }
+        switch (*++p) {
+            case 'd':
+                ival = va_arg(ap, int);
+                printf("%d", ival);
+                break;
+            case 'f':
+                dval = va_arg(ap, double);
+                printf("%f", dval);
+                break;
+            case 's':
+                for (sval = va_arg(ap, char *); *sval; sval++)
+                    putchar(*sval);
+                break;
+            default:
+                putchar(*p);
+                break;
+        }
+    }
+    va_end(ap); /*Clean up when done */
+
 }
 
 int main(void) {
-    printf("Minrpint!\n");
+    char *s = "Variable-length Argument List";
+    int world = 100;
+    minprintf("Hello %d, this is %s", world, s);
     return 0;
 }
