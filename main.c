a91ac2c1f90945ccf7ae8d0bdb2b29dc03831975
guramnozadze <gukanozadze@gmail.com>
2026-06-25 20:04:26 +0400

    project scaffold

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/* minprintf: minimal printf with variable argument list */
+void minprintf(char *fmt, ...) {
+    va_list ap; /* points to each unnamed arg in turn */
+        char *p, *sval;
+}
+
+int main(void) {
+    printf("Minrpint!\n");
+    return 0;
+}
